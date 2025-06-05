bcc86c371ee9968a4e949aeb620d936dc7e392fd
Federico <fededeniard@gmail.com>
2025-06-05 17:55:25 -0300

    amplify-auth

---
+++ src/static/frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "musescorego5a2fda46": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
